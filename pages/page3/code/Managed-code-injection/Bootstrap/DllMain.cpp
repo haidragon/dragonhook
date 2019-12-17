dfa787293c0ae54213c6abdce0f9c79217ede021
饭醉分子 <18684797674@163.com>
2019-12-17 09:46:22 +0800

    Add files via upload

---
+++ pages/page3/code/Managed-code-injection/Bootstrap/DllMain.cpp
@@ -0,0 +1,142 @@
+#include <Windows.h>
+#include <mscoree.h>
+#include <metahost.h>
+#include <wchar.h>
+#include <fstream>
+
+#pragma comment(lib, "mscoree.lib")
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+
+static const LPCTSTR Log      = "LogFile.log";
+static const LPCWSTR Assembly = L"C://Users//22073//Desktop//work//Managed-code-injection-master//CodeInject//bin//Debug//CodeInject.dll";
+static const LPCWSTR Class    = L"CodeInject.Startup";
+static const LPCWSTR Method   = L"EntryPoint";
+static const LPCWSTR Param    = L"";
+
+
+DWORD WINAPI CreateDotNetRunTime(LPVOID lpParam)
+{
+	ICLRRuntimeHost* lpRuntimeHost = NULL;
+	ICLRRuntimeInfo* lpRuntimeInfo = NULL;
+	ICLRMetaHost* lpMetaHost = NULL;
+	FILE* file;
+
+	LPWSTR AppPath = new WCHAR[_MAX_PATH];
+	::GetModuleFileNameW((HINSTANCE)&__ImageBase, AppPath, _MAX_PATH);
+
+	std::wstring tempPath = AppPath;
+	int index = tempPath.rfind('\\');
+	tempPath.erase(index, tempPath.length() - index);
+	tempPath = Assembly;
+
+	fopen_s(&file, Log, "a+");
+
+	HRESULT hr = CLRCreateInstance(
+		CLSID_CLRMetaHost, 
+		IID_ICLRMetaHost, 
+		(LPVOID*)&lpMetaHost
+	);
+
+	if (FAILED(hr))
+	{
+		fprintf(file, "Failed to create CLR instance.\n");
+		fflush(file);
+	}
+
+	hr = lpMetaHost->GetRuntime(
+		L"v4.0.30319", 
+		IID_PPV_ARGS(&lpRuntimeInfo)
+	);
+
+	if (FAILED(hr))
+	{
+		fprintf(file, "Getting runtime failed.\n");
+		fflush(file);
+
+		lpMetaHost->Release();
+	}
+
+	BOOL fLoadable;
+	hr = lpRuntimeInfo->IsLoadable(&fLoadable);
+
+	if (FAILED(hr) || !fLoadable)
+	{
+		fprintf(file, "Runtime can't be loaded into the process.\n");
+		fflush(file);
+
+		lpRuntimeInfo->Release();
+		lpMetaHost->Release();
+	}
+
+	hr = lpRuntimeInfo->GetInterface(
+		CLSID_CLRRuntimeHost, 
+		IID_PPV_ARGS(&lpRuntimeHost)
+	);
+
+	if (FAILED(hr))
+	{
+		fprintf(file, "Failed to acquire CLR runtime.\n");
+		fflush(file);
+
+		lpRuntimeInfo->Release();
+		lpMetaHost->Release();
+	}
+
+	hr = lpRuntimeHost->Start();
+
+	if (FAILED(hr))
+	{
+		fprintf(file, "Failed to start CLR runtime.\n");
+		fflush(file);
+
+		lpRuntimeHost->Release();
+		lpRuntimeInfo->Release();
+		lpMetaHost->Release();
+	}
+
+	DWORD dwRetCode = 0;
+
+	hr = lpRuntimeHost->ExecuteInDefaultAppDomain(
+		(LPWSTR)tempPath.c_str(), 
+		Class,
+		Method, 
+		Param, 
+		&dwRetCode
+	);
+
+	if (FAILED(hr))
+	{
+		fprintf(file, "Unable to execute assembly.\n");
+		fflush(file);
+
+		lpRuntimeHost->Stop();
+		lpRuntimeHost->Release();
+		lpRuntimeInfo->Release();
+		lpMetaHost->Release();
+	}
+
+	fclose(file);
+
+	return 0;
+}
+
+DWORD APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, NULL, CreateDotNetRunTime, NULL, NULL, NULL);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		//MessageBoxA(NULL, "ok", "ok", 0);
+	default:
+		break;
+	}
+	return true;
+}
+int main() {
+	CreateThread(NULL, NULL, CreateDotNetRunTime, NULL, NULL, NULL);
+	return 0;
+}
---
+++ pages/page3/code/Managed-code-injection/CodeInject/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CodeInject
+{
+    internal static class Startup
+    {
+        #region Private Fields
+
+        private static bool m_initialized;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        [STAThread]
+        private static int EntryPoint(string args)
+        {
+            MessageBox.Show($"Managed code running inside [{Process.GetCurrentProcess().ProcessName}]");
+            if (m_initialized)
+                return 0;
+
+            m_initialized = true;
+
+            MessageBox.Show($"Managed code running inside [{Process.GetCurrentProcess().ProcessName}]");
+
+            return 0;
+        }
+
+        #endregion Private Methods
+    }
+}
